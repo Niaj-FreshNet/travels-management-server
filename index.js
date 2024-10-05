838304108c57c35809ee59ba08d4ff1324d4dedb
Niaj-FreshNet <khushbuwaala@gmail.com>
2024-10-06 01:03:25 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: "https://travels-management.web.app"
+}));
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kqlaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
