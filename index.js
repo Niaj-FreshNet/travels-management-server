f5c4613114af08fdd6243d82ac8addbbcd5065ab
Niaj-FreshNet <khushbuwaala@gmail.com>
2024-10-07 02:22:57 +0600

    added localhost in origin

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://travels-management.web.app"
+  origin: [
+    'https://travels-management.web.app',
+    'http://localhost:5173'
+  ]
 }));
 
 
