b2df636d3b54c33ab59b388290262605f30f6e97
Niaj-FreshNet <khushbuwaala@gmail.com>
2024-11-04 10:13:50 +0600

    committed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Use if you are using cookies or authentication headers
+  credentials: true
 }));
 
 
